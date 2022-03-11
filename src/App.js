ded3c84c7eb6b944b4aa802b872f754c7cb89818
Diego Nunes <nunesdiegond@gmail.com>
2022-03-11 14:08:01 -0300

    inserido botão para deletar a nota

--- src/App.js
+++ src/App.js
@@ -21,11 +21,21 @@ class App extends Component {
     this.setState(novoEstado)
   }
 
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas:arrayNotas})
+//    console.log("Deletando");
+  }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas 
+          notas={this.state.notas}
+          apagarNota={this.deletarNota.bind(this)}
+        />
       </section>
     );
   }
