1e9feaeec6b7f40a260e1d87d2d883a08cf713f3
Diego Nunes <nunesdiegond@gmail.com>
2022-03-13 17:01:15 -0300

    inserindo categorias de notas, part 1 campo e localização

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaCategorias from "./components/ListaCategorias";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
@@ -32,10 +33,13 @@ class App extends Component {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas 
-          notas={this.state.notas}
-          apagarNota={this.deletarNota.bind(this)}
-        />
+        <main className="conteudo-principal">
+          <ListaCategorias />
+          <ListaDeNotas 
+            notas={this.state.notas}
+            apagarNota={this.deletarNota.bind(this)}
+          />
+        </main>
       </section>
     );
   }
---
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ListaCategorias extends Component {
+    
+    render() { 
+        return ( 
+            <section>
+                <ul>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                    <li>Categoria</li>
+                </ul>
+                <input type="text" />
+            </section>
+         );
+    }
+}
+ 
+export default ListaCategorias;
